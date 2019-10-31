3ada4bf68eed61e520864b09cb81c9be8bace833
FoseFx <fosefx@pm.me>
2019-10-31 08:38:52 +0100

    Adds Styling and drop-box

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from 'rollup-plugin-typescript';
 import {terser} from 'rollup-plugin-terser';
 import html from 'rollup-plugin-bundle-html';
+import copy from 'rollup-plugin-copy'
 
 export default [
 	{
@@ -11,7 +12,12 @@ export default [
 			format: 'iife'
 		},
 		plugins: [
-            typescript(),
+			typescript(),
+			copy({
+				targets: [
+					{src: 'src/main.css', dest: 'dist'}
+				]
+			}),
 			html({
 				template: 'src/index.html',
 				dest: "dist",
